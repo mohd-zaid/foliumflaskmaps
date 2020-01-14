0b25b546082ff0be3dd0dfe2b83bcea880f06a3f
Mohammad Abbasi <amaanabbasi99@gmail.com>
2020-01-14 23:24:33 +0530

    Added Flask

--- map2.py
+++ map2.py
@@ -1,25 +1,32 @@
 import folium
 import os
 import pandas as pd
+import flask
 
+@app.route('/')
+def index():
+  indianStates = os.path.join('data1', 'indiaStates.json')
+  Protestf = os.path.join('data1', 'geoMap.csv')
 
-indianStates = os.path.join('data1', 'indiaStates.json')
-Protestf = os.path.join('data1', 'geoMap.csv')
+  searchRes = pd.read_csv(Protestf)
 
-searchRes = pd.read_csv(Protestf)
+  # print(searchRes)
+  map = folium.Map(location=[21.7679,  78.8718],
+                   zoom_start=5.4, tiles='openstreetmap')
+  folium.Choropleth(
+      geo_data=indianStates,
+      name='Protest Rate',
+      data=searchRes,
+      columns=['Region', 'Protest'],
+      key_on='feature.properties.NAME_1',
+      fill_color='PuBu',
+      fill_opacity=1,
+      line_opacity=1,
+      nan_fill_color='#f2efe'
+  ).add_to(map)
+  map.save('index1.html')
+  
+return map._repr_html_()
 
-# print(searchRes)
-map = folium.Map(location=[21.7679,  78.8718],
-                 zoom_start=5.4, tiles='openstreetmap')
-folium.Choropleth(
-    geo_data=indianStates,
-    name='Protest Rate',
-    data=searchRes,
-    columns=['Region', 'Protest'],
-    key_on='feature.properties.NAME_1',
-    fill_color='PuBu',
-    fill_opacity=1,
-    line_opacity=1,
-    nan_fill_color='#f2efe'
-).add_to(map)
-map.save('index1.html')
+if '__name__' == '__main__':
+  app.run(debug=True)
